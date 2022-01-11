046ed1122ae45e9ee1fc5b3314543532b85605e7
Qabas al ani <doctorqa@yahoo.com>
2022-01-11 10:29:12 -0500

    added the banner for rent

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,16 @@ export default function Home() {
   return (
     <div>
       <h1>Home sweet Home</h1>
-      <Banner />
+      <Banner
+        purpose="RENT A HOME "
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes "
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
     </div>
   );
 }
